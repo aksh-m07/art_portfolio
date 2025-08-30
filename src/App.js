72138086dbf11bb5ce58d7546d65084822d2d005
Aksh Malik <akshmalik@Akshs-MacBook-Pro.local>
2025-08-30 12:40:50 +0530

    updated home icon link v2

--- src/App.js
+++ src/App.js
@@ -825,7 +825,7 @@ function AppContent() {
           <div className="footer-right">
             
             <div className="social-icons">
-              <a href="akshmalik.me" className="social-icon" title="Home">
+              <a href="https://akshmalik.me/" className="social-icon" title="Home">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                   <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/>
                 </svg>
